db923d4ffd4a933c0a595faf49e22960de5b3717
Jonas Karlman <jonas@kwiboo.se>
2016-02-22 03:40:49 +0100

    Add breakpad

---
+++ depends/breakpad/breakpad.bat
@@ -0,0 +1,22 @@
+SETLOCAL
+
+set DEPEND_NAME=breakpad
+set DEPEND_VERSION=chrome_49
+set DEPEND_PACKAGE=%DEPEND_NAME%-%DEPEND_VERSION%
+set DEPEND_URL=https://chromium.googlesource.com/breakpad/breakpad/+archive/%DEPEND_VERSION%.tar.gz
+
+IF EXIST %DEPEND_PACKAGE% rmdir /S /Q %DEPEND_PACKAGE%
+%WGET% --no-check-certificate "%DEPEND_URL%"
+%ZIP% x -y %DEPEND_VERSION%.tar.gz
+del /F /Q %DEPEND_VERSION%.tar.gz
+%ZIP% x -y -o%DEPEND_PACKAGE% %DEPEND_VERSION%.tar
+del /F /Q %DEPEND_VERSION%.tar
+cd %DEPEND_PACKAGE%
+
+%PATCH% -p1 < ../cmake.patch
+
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=%BUILD_PATH% ..
+nmake install
