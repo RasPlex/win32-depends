3df779ed4279ab876ef9a7b80ce4ca3bc0979be4
Jonas Karlman <jonas@kwiboo.se>
2018-04-14 18:07:47 +0200

    Bump curl to 7.58.0

--- depends/curl/curl.bat
+++ depends/curl/curl.bat
@@ -3,15 +3,15 @@ SETLOCAL
 set DEPEND_NAME=curl
 set DEPEND_VERSION=7.46.0
 if "%VS%" == "12" (
-  set DEPEND_VERSION=7.52.1
+  set DEPEND_VERSION=7.58.0
 )
 set DEPEND_PACKAGE=%DEPEND_NAME%-%DEPEND_VERSION%
-set DEPEND_URL=http://curl.haxx.se/download/%DEPEND_PACKAGE%.tar.lzma
+set DEPEND_URL=http://curl.haxx.se/download/%DEPEND_PACKAGE%.tar.gz
 
 IF EXIST %DEPEND_PACKAGE% rmdir /S /Q %DEPEND_PACKAGE%
 %WGET% --no-check-certificate "%DEPEND_URL%"
-%ZIP% x -y %DEPEND_PACKAGE%.tar.lzma
-del /F /Q %DEPEND_PACKAGE%.tar.lzma
+%ZIP% x -y %DEPEND_PACKAGE%.tar.gz
+del /F /Q %DEPEND_PACKAGE%.tar.gz
 %ZIP% x -y %DEPEND_PACKAGE%.tar
 del /F /Q %DEPEND_PACKAGE%.tar
 cd %DEPEND_PACKAGE%
